5e8bc0bd3819d5572c7f71f16f7868e7fd8a28b1
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 11:26:50 -0600

    determinar el promedio de una serie de numeros

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -10,6 +10,22 @@ namespace Mi_primer_proyecto
     {
         static void Main(string[] args)
         {
+            //Prioridad de los operadores aritméticos.
+            //ejercicio de obtener la nota final de la materia de Programacion I.
+            Consola.Escribir(" Lab1 : ");​
+            doble lab1 = doble.Parse(Console.ReadLine());  //8
+
+            Consola.Escribir(" Lab2 : ");​
+            doble lab2 = doble.Parse(Console.ReadLine());  //9
+
+            Consola.Escribir(" Parcial 1: ");
+            doble parcial1 = doble.Parse(Console.ReadLine());  //7
+
+            // 8*30%= 2,4
+            // 9*30%= 2,7
+            // 7*40%= 2,8
+            //C1 = 7,9
+
             doble c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
             Consola.WriteLine(" La nota de C1 es: {0} ", c1);
 
@@ -28,6 +44,14 @@ namespace Mi_primer_proyecto
             //TAREA: Obtener la nota final por computo y Nota Final de la matera de Programacion I.
 
 
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 };  //32
+            int suma = 0;
+            foreach (int num en serie ) {
+                suma += num;
+            }
+            prom  decimal = suma / serie.Longitud;
+            Consola.WriteLine(" La suma es: {0}, el promedio {1} ", suma, prom);
             //Pausa.
             Consola.ReadLine();
         }
