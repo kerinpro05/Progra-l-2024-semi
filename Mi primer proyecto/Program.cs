5ae83c0b426efe4ac765e2ad66518ae4f0d0842a
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 11:33:04 -0600

    1. si. ejercicio. pedir al usuario la edad si es mayor de edad que le diga diga bienvenido sino imprime el mensaje de Eresun adolescente

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,49 +12,27 @@ namespace Mi_primer_proyecto
         static void Main(string[] args)
         {
             //Prioridad de los operadores aritméticos.
-            //ejercicio de obtener la nota final de la materia de Programacion I.
-            Consola.Escribir(" Lab1 : ");​
-            doble lab1 = doble.Parse(Console.ReadLine());  //8
-
-            Consola.Escribir(" Lab2 : ");​
-            doble lab2 = doble.Parse(Console.ReadLine());  //9
-
-            Consola.Escribir(" Parcial 1: ");
-            doble parcial1 = doble.Parse(Console.ReadLine());  //7
-
-            // 8*30%= 2,4
-            // 9*30%= 2,7
-            // 7*40%= 2,8
-            //C1 = 7,9
-
-            doble c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Consola.WriteLine(" La nota de C1 es: {0} ", c1);
-
-            Consola.Escribir(" Lab1 : ");​
-            lab1 = double.Parse(Console.ReadLine());  //8
-
-            Consola.Escribir(" Lab2 : ");​
-            lab2 = double.Parse(Console.ReadLine());  //9
-
-            Consola.Escribir(" Parcial 1: ");
-            parcial1 = double.Parse(Console.ReadLine());  //7
-
-            doble c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Consola.WriteLine(" La nota de C2 es: {0} ", c2);
-
-            //TAREA: Obtener la nota final por computo y Nota Final de la matera de Programacion I.
-
-
             //ejercicio obtener el promedio de una serie de numeros
             int[] serie = new int[] { 5, 4, 6, 8, 9 };  //32
             int suma = 0;
             foreach (int num en serie ) {
                 suma += num;
+                //estructuras de control.
+                //1. si. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
+                Consola.Write(" Edad: ");
+                int edad = int.Parse(Console.ReadLine());​​
+
+            si(edad >= 18)  {
+                    Consola.WriteLine(" Bienvenido al mundo de las reposabilidades. ");
+                }
+                demás  {
+                    Consola.WriteLine(" Eres un adolescente, disfruta de la vida. ");
+                }
+                prom  decimal = suma / serie.Longitud;
+                Consola.WriteLine(" La suma es: {0}, el promedio {1} ", suma, prom);
+
+                //Pausa.
+                Consola.ReadLine();
             }
-            prom  decimal = suma / serie.Longitud;
-            Consola.WriteLine(" La suma es: {0}, el promedio {1} ", suma, prom);
-            //Pausa.
-            Consola.ReadLine();
-        }
     }
-                
+                            
