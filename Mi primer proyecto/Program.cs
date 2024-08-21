e17ca9ee61dd760bcaf1112956bed707a034c444
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 11:01:08 -0600

    tipos de datos en c#

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -11,22 +11,25 @@ namespace Mi_primer_proyecto
         static void Main(string[] args)
         {
             Console.Write(" Num1 : ");​
-            int num1 = int.Parse(Console.ReadLine());​​​
-            sbyte num1 = sbyte.Parse(Console.ReadLine());​​​
+            sbyte num1 = sbyte.Parse(Console.ReadLine());​​​   
+            Booleano comprobar = true;
+            Console.WriteLine(" El valor es: {0} ", comprobar);
 
-            Console.Write(" Num 2 : ");​
-            int num2 = int.Parse(Console.ReadLine());
             Console.Write(" Num2 : ");​​
-            sbyte num2 = sbyte.Parse(Console.ReadLine());​​​
+            sbyte num2 = sbyte.Parse(Console.ReadLine());​​​   
+            Console.Write(" Nombre : ");
+            cadena nombre = Console.ReadLine();​​  
 
-            int respuesta = num1 + num2;
-            Console.WriteLine(" La suma de {0} + {1} = {2} ", num1, num2, respuesta);
             int suma = num1 + num2;
 
             Console.WriteLine(" La suma de {0} + {1} = {2} ", num1, num2, suma);
+            Console.WriteLine(" Hola {0} bienvenido ", nombre);
 
+            FechaHora fechaActual = new DateTime();
+            fechaActual = DateTime.Now;
+            Console.WriteLine(" La fecha actual es: {0} ", fechaActual);
             //Pausa.
-            Console.ReadLine();​
+            Console.ReadLine();
 
         }
     }
