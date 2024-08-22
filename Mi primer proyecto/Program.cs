a21cf9293dfc4a44513f129113c8c6377d390657
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 18:54:30 -0600

    ejercicio encontrar la media aritmetica y la desviacion tipica de una serie de numeros

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -12,8 +12,7 @@ namespace Mi_primer_proyecto
         static void Main(string[] args)
         {
 
-            //Uso de funciones...
-            Consola.WriteLine(" La suma de 10+5={0} ", suma());
+            //ejercicio encontrar la media aritmetica y la desviación típica de una serie de números.
 
             Consola.Escribir(" Num 1 : ");
             int num1 = int.Parse(Console.ReadLine());​​​
@@ -22,6 +21,8 @@ namespace Mi_primer_proyecto
             int num2 = int.Parse(Console.ReadLine());
 
             Consola.WriteLine(" La suma de {0}+{1}={2} ", num1, num2, suma(num1, num2));
+            int[] serie = new int[] { 1, 2, 3, 4, 5 };
+            Consola.WriteLine(" La media aritmetica es: {0}, y la desviación típica es: {1} ", media(serie), tipica(serie));
 
             Consola.ReadLine();​
         }
@@ -29,14 +30,26 @@ namespace Mi_primer_proyecto
         {
             int respuesta = 10 + 5;
             devolver respuesta;
-        }
-        int estático suma(int a, int b)
-        {
-            int respuesta = a + b;
-            devolver respuesta;
+            media doble  estática(int[ ] serie) {
+                doble media = 0;
+                foreach (int num en serie )  {
+                    medios += num;
+                }
+                retorno medio / serie.Longitud;
+            }
+            int estático suma(int  a, int  b)  {
+                int respuesta = a + b;
+                devolver respuesta;
+                tipica doble  estática(int[ ] serie) {
+                    doble tipica = 0,
+                            m = media(serie);
+                    foreach (int num en serie )  {
+                        típica += Matemáticas.Pow(núm - m, 2);
+                    }
+                    típica = Matemáticas.Sqrt(tipica / serie.Longitud);
+                    devuelve Math . Round(tipica, 2);
 
-
-        }
+                }
 
         
 
