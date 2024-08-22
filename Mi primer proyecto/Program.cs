cfb71fc4ce11fcb09919c987b9e8547f2c2aba7f
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 19:00:09 -0600

    Cálculo de la media armónica

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -12,42 +12,15 @@ namespace Mi_primer_proyecto
         static void Main(string[] args)
         {
 
-            //ejercicio encontrar la media aritmetica y la desviación típica de una serie de números.
-
-            int[] serie = new int[] { 1, 2, 3, 4, 5 };
-            Consola.WriteLine(" La media aritmetica es: {0}, y la desviación típica es: {1} ", media(serie), tipica(serie));
-            Consola.Write(" Ingrese la serie de números separados por comas: ");
-            Cadena serie = Console.ReadLine();​​
-            Cadena[] numeros = serie.Dividir(',');
-
-            Consola.WriteLine(" La media aritmetica es: {0}, y la desviacion tipica es: {1} ", media(numeros), tipica(numeros));
-
-            Consola.ReadLine();​
-        }
-        media doble  estática(int[] serie)
-        {
-            media doble  estática(cadena[ ] serie) {
-                doble media = 0;
-                foreach (int num en serie )  {
-                    medios += num;
-                    foreach (cadena num en serie )  {
-                        media += int.Parse(num);
-                    }
-                    retorno medio / serie.Longitud;
+            armónica doble  estática(cadena[ ] serie) {
+                doble armónica = 0;
+                foreach (cadena num en serie )  {
+                    armónica += (1.0 / double.Parse(num));
                 }
-                tipica doble  estática(int[ ] serie) {
-                    estática doble  típica(cadena[ ]  serie)  {
-                        doble tipica = 0,
-                                m = media(serie);
-                        foreach (int num en serie )  {
-                            típica += Matemáticas.Pow(núm - m, 2);
-                            foreach (cadena num en serie )  {
-                                típica += Matemáticas.Pow(int.Parse(núm) - m, 2);​
-            }
-                            típica = Matemáticas.Sqrt(tipica / serie.Longitud);
-                            devuelve Math . Round(tipica, 2);
+                armónica = serie.Longitud / armónica;
+                devuelve Math . Round(armónica, 2);
 
-                        }
+            }
 
         
 
