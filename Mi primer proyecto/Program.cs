bf5ece3c6b8f5a2706b684ed5ada666ad38d53f6
kerinpro05 <kerinrodriguez50398@gmail.com>
2024-08-21 18:19:28 -0600

    uso de matriz bidimensional

--- Mi primer proyecto/Program.cs
+++ Mi primer proyecto/Program.cs
@@ -11,120 +11,23 @@ namespace Mi_primer_proyecto
     {
         static void Main(string[] args)
         {
-            //estructuras de control.
-            //2. Cambiar de ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
-            cadena continuar = " s ";
-            mientras(continuar == " s ")  {
-                Consola.WriteLine(" \ n \n *** MENÚ *** ");
-                Consola.WriteLine(" 1. Promedio Notas ");
-                Consola.WriteLine(" 2. Promedio Serie Numeros ");
-                Consola.WriteLine(" 3. Clasificación edad ");
-                Consola.WriteLine(" 4. Tabla Multiplicar ");
-                Consola.WriteLine(" 5.Salir ");​
-                Consola.Write(" Opcion: ");
-                int opcion = int.Parse(Console.ReadLine());​​​
-                Consola.Claro(); //limpiar la consola
-                cambiar(opcion)  {
-                    caso  1 : //si(opcion==1)
-                        promedio();
-                    romper;
-                    caso  2 :  //si(opcion==2)
-                        promedioSerie();
-                    romper;
-                    caso  3 : //si(opcion==3)
-                        clasificacionEdad();
-                    romper;
-                    caso  4 :
-                        tablaMultiplicar();
-                    romper;
-                    caso  5 : //si(opcion==5)
-                        continuar = " n ";
-                    romper;
-                predeterminado: //de lo contrario
-                    Console.WriteLine(" opción incorrecta \n \n ");
-                    romper;
-                }
-            }
-        }
-        void estático tablaMultiplicar()
-        {
-            Consola.Escribir(" Tabla: ");
-            int ntabla = int.Parse(Console.ReadLine());​​​
-            para(int  i = 1 ; i <= 10; i++ )  { //i++ => i = i + 1
-                Consola.WriteLine(" {0}x{1} = {2} ", ntabla, i, ntabla * i);
-            }
-        }
-        void estático promedio()
-        {
-            Consola.Escribir(" Lab1 : ");​
-            doble lab1 = doble.Parse(Console.ReadLine());
-
-            Consola.Escribir(" Lab2 : ");​
-            doble lab2 = doble.Parse(Console.ReadLine());
-
-            Consola.Escribir(" Parcial 1: ");
-            doble parcial1 = doble.Parse(Console.ReadLine());
-
-            doble c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Consola.WriteLine(" La nota de C1 es: {0} ", c1);
-
-            Consola.Escribir(" Lab1 : ");​
-            lab1 = double.Parse(Console.ReadLine());  //8
-
-            Consola.Escribir(" Lab2 : ");​
-            lab2 = double.Parse(Console.ReadLine());  //9
-
-            Consola.Escribir(" Parcial 1: ");
-            parcial1 = double.Parse(Console.ReadLine());  //7
-
-            doble c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Consola.WriteLine(" La nota de C2 es: {0} ", c2);
-        }
-        void estático promedioSerie()
-        {
-            int[] serie = new int[] { 5, 4, 6, 8, 9 };  //32
-            int suma = 0;
-            foreach (int num en serie )  {
-                suma += num;
-            }
-            prom  decimal = suma / serie.Longitud;
-            Consola.WriteLine(" La suma es: {0}, el promedio {1} ", suma, prom);
-        }
-        static void clasificacionEdad()
-        {
-            Consola.Write(" Edad: ");
-            int edad = int.Parse(Console.ReadLine());​​
-
-            si(edad < 0)  {
-                Consola.WriteLine(" Edad incorrecta. ");
-            }
-            de lo contrario si(edad <= 2 )  {
-                Consola.WriteLine(" Eres un bebe ");
-            }
-            de lo contrario si(edad<  12 )  {
-                Consola.WriteLine(" Eres un niño ");
-            }
-            de lo contrario si(edad<  18 )  {
-                Consola.WriteLine(" Eres un adolescente. ");
-            }
-            de lo contrario si(edad <= 65 )  {
-                Consola.WriteLine(" Bienvenido al mundo de las reposabilidades. ");
-            }
-            de lo contrario si(edad <= 80 )  {
-                Consola.WriteLine(" Eres un adulto mayor ");
-            }
-            demás  {
-                Consola.WriteLine(" Larga vida ");
-            }
             //Uso de matrices.
             int[] numeros = nuevo  int[]  { 2 , 8 , 9 , 10 , 1 , 3 };
-            Consola.WriteLine(" El numero de la posicion 3 es {0} ", numeros[3]);
-            //Pausa
-            Consola.ReadKey();
+            para(int  i = 0 ; i < numeros.Longitud; i++ )  {
+                Consola.WriteLine(" Índice: {0}= {1} ", i, numeros[i]);
+                int[,] matriz = nuevo  int[,]  {
+                    { 5 , 4 , 5 , 2 , 1 } ,
+                { 1 , 2 , 8 , 9 , 3 }
+                };
+                para(int  i = 0 ; i < 2; i++ )  {
+                    para(int  j = 0 ; j < 5; j++ )  {
+                        Consola.Escribir(" {0} ", matriz[i, j]);
+                    }
+                    Consola.WriteLine(" \ n ");
+                }
+                //Pausa
+                Consola.ReadKey();
 
-
-
-
-        }
+            }
     }
-                                               
+                                                  
